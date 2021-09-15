fafdcc138d6cb1030483c4544e6d785936b9b1f1
MaxLi92 <m.li.92@icloud.com>
2021-09-15 19:59:26 +0600

    Initial Commit

---
+++ HW_course_6/HW_course_6App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_course_6App.swift
+//  HW_course_6
+//
+//  Created by Max Li on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW_course_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
